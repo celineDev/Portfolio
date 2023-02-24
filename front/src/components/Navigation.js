7bc1e2c28ba88dc2012a855896a6f982d78d3cce
Celine Mouflard <celine667@hotmail.fr>
2023-02-24 18:00:23 +0100

    corrected deprecated version of index, replace burger icon by css

--- front/src/components/Navigation.js
+++ front/src/components/Navigation.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HashLink } from 'react-router-hash-link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-import { faXmark } from '@fortawesome/free-solid-svg-icons'
 
 const Navigation = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleMenu = () => {
+        const header = document.querySelector('header')
+
+        if (header.classList.contains('open')) {
+            header.classList.remove('open')
+        } else {
+            header.classList.add('open')
+        }
+        setIsOpen(!isOpen)
+    }
+
     return (
         <header>
-            <a className='logo' href="#">cm</a>
+            <a className='logo' href="/">cm</a>
             <nav>
                 <ul className='link hide-for-mobile'>
                     <li><HashLink smooth to="#accueil">Accueil</HashLink></li>
@@ -18,21 +28,22 @@ const Navigation = () => {
                     <li><HashLink smooth to="#contact">Contact</HashLink></li>
                 </ul>
 
-                <button className='burger-btn closed'>
-                    <div><FontAwesomeIcon icon={faBars} size="2x" /></div>
+                <button onClick={handleMenu} className='burger-btn'>
+                    <span></span>
+                    <span></span>
+                    <span></span>
                 </button>
-                {/* <button className='burger-menu'> <div>
-                        <FontAwesomeIcon icon={faXmark} size="2x" />
-                        </div>
-                    </button> */}
             </nav>
-            <ul className='toggle hide-for-desktop has-fade'>
-                <li><HashLink smooth to="#accueil">Accueil</HashLink></li>
-                <li><HashLink smooth to="#apropos">A propos</HashLink></li>
-                <li><HashLink smooth to="#competences">Compétences</HashLink></li>
-                <li><HashLink smooth to="#portfolio">Portfolio</HashLink></li>
-                <li><HashLink smooth to="#contact">Contact</HashLink></li>
-            </ul>
+
+            {isOpen && (
+                <ul className='toggle hide-for-desktop has-fade'>
+                    <li><HashLink smooth to="#accueil">Accueil</HashLink></li>
+                    <li><HashLink smooth to="#apropos">A propos</HashLink></li>
+                    <li><HashLink smooth to="#competences">Compétences</HashLink></li>
+                    <li><HashLink smooth to="#portfolio">Portfolio</HashLink></li>
+                    <li><HashLink smooth to="#contact">Contact</HashLink></li>
+                </ul>
+            ) }
         </header>
     );
 };
