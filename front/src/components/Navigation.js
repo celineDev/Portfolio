fa8c588c3448a64e8b6babc884adf5044d12e084
Celine Mouflard <celine667@hotmail.fr>
2023-02-28 17:13:08 +0100

    Navigation menu is done

--- front/src/components/Navigation.js
+++ front/src/components/Navigation.js
@@ -18,32 +18,34 @@ const Navigation = () => {
 
     return (
         <header>
-            <a className='logo' href="/">cm</a>
-            <nav>
-                <ul className='link hide-for-mobile'>
-                    <li><HashLink smooth to="#accueil">Accueil</HashLink></li>
-                    <li><HashLink smooth to="#apropos">A propos</HashLink></li>
-                    <li><HashLink smooth to="#competences">Compétences</HashLink></li>
-                    <li><HashLink smooth to="#portfolio">Portfolio</HashLink></li>
-                    <li><HashLink smooth to="#contact">Contact</HashLink></li>
-                </ul>
+            <div className='nav-container'>
+                <a className='logo' href="/">cm</a>
+                <nav>
+                    <ul className='link hide-for-mobile'>
+                        <li><HashLink smooth to="#accueil">Accueil</HashLink></li>
+                        <li><HashLink smooth to="#apropos">A propos</HashLink></li>
+                        <li><HashLink smooth to="#competences">Compétences</HashLink></li>
+                        <li><HashLink smooth to="#portfolio">Portfolio</HashLink></li>
+                        <li><HashLink smooth to="#contact">Contact</HashLink></li>
+                    </ul>
 
-                <button onClick={handleMenu} className='burger-btn'>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </button>
-            </nav>
+                    <button onClick={handleMenu} className='burger-btn'>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
+                </nav>
+            </div>
 
             {isOpen && (
-                <ul className='toggle hide-for-desktop has-fade'>
+                <ul className='toggle hide-for-desktop'>
                     <li><HashLink smooth to="#accueil">Accueil</HashLink></li>
                     <li><HashLink smooth to="#apropos">A propos</HashLink></li>
                     <li><HashLink smooth to="#competences">Compétences</HashLink></li>
                     <li><HashLink smooth to="#portfolio">Portfolio</HashLink></li>
                     <li><HashLink smooth to="#contact">Contact</HashLink></li>
                 </ul>
-            ) }
+            )}
         </header>
     );
 };
