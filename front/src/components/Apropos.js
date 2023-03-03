2106382f50115bda894129106187800261f588e2
Celine Mouflard <celine667@hotmail.fr>
2023-03-03 17:59:29 +0100

    finished section a-propos

--- front/src/components/Apropos.js
+++ front/src/components/Apropos.js
@@ -1,21 +1,16 @@
 import React from 'react';
+import cv from './../assets/cv/cv.pdf'
 
 const Apropos = () => {
     return (
-        <div>
-            <h2 id="apropos">a propos</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur obcaecati sequi sed eius id consequatur autem, delectus voluptatem harum mollitia non dignissimos modi similique quae nemo incidunt voluptates amet et.
-            Commodi labore eos nihil distinctio ducimus non porro. Odio, inventore nobis, minus obcaecati est excepturi veritatis quis tempora quisquam molestiae enim deleniti, quia impedit ipsam ad at alias? Id, cumque.
-            Nisi corporis est aut explicabo facere, eum suscipit consectetur maiores illum repellat, quae consequuntur. Consequatur nihil deleniti perspiciatis obcaecati, veniam tempore amet at nobis reiciendis, esse, ipsa commodi veritatis maxime?
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda, illo quaerat. Nisi iste cum voluptatem aut dolor. Quam, molestias! Quo sit veniam distinctio vero repellendus voluptas doloribus velit et eaque?
-            Quisquam quam quas architecto minima facilis magni rerum nulla quos sunt, possimus cumque similique recusandae, provident error temporibus? Iure dicta voluptatibus voluptas voluptatem illum magnam totam explicabo, rem corporis tenetur?
-            Cumque, possimus. Autem aspernatur obcaecati expedita nostrum asperiores optio, laborum quam dignissimos excepturi quos omnis, libero perspiciatis maiores id facilis a nulla eos architecto sed? Quasi est earum vel veniam.
-            Odit doloremque odio fugiat dignissimos magni obcaecati, soluta architecto ab esse doloribus praesentium temporibus laborum impedit sapiente animi quidem dolor rerum illum quod aspernatur fugit ea asperiores quisquam! Repudiandae, omnis.
-            Sapiente, enim, pariatur non omnis qui animi accusantium doloribus id perspiciatis, nobis repellendus et quia exercitationem quis. Culpa minus id quisquam ipsa? Incidunt laboriosam quasi quas necessitatibus fugiat quo est?
-            Officiis sit dolores non corrupti suscipit magnam excepturi laboriosam voluptatem esse aperiam minus iusto voluptatibus natus est quaerat hic, nemo pariatur fuga provident. Dolorem error architecto similique officia harum nostrum.
-            Eius porro molestiae quibusdam, aliquam quaerat repellendus temporibus unde suscipit culpa dolorem aperiam dignissimos aspernatur doloremque possimus, illo rem assumenda impedit ipsam itaque deserunt beatae! Nihil consectetur eum cum veritatis.
-            Architecto libero modi, voluptatibus, repellat voluptate veritatis deserunt magni mollitia sapiente cupiditate dolore impedit incidunt officia voluptas quia quaerat vel aperiam praesentium similique soluta nobis deleniti quae accusantium! Expedita, sequi?</p>
-        </div>
+        <section id="apropos">
+            <h2>à-propos de moi<span>qui suis-je ?</span></h2>
+            <div className="separation-line"></div>
+            <p>Passionnée par le développement web, je suis particulièrement intéressée par le front-end et le framework React JS. Pour élargir mes compétences, je continue à me former quotidiennement sur ce framework.
+            J'ai comme ambition de progresser et d'élargir mes compétences afin de rester à la pointe de l'innovation en terme de compétences.
+            </p>
+            <button><a target="_blank" rel="noreferrer" href={cv}>Télécharger mon CV</a></button>
+        </section>
     );
 };
 
