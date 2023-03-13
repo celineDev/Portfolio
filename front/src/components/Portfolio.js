941e1e96fbbf106c624580b6b911f47ac2cff1d5
Celine Mouflard <celine667@hotmail.fr>
2023-03-13 16:58:17 +0100

    Complete portfolio part

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -1,14 +1,89 @@
 import React from 'react';
+import booki from './../assets/projets/booki.png'
+import ohmyfood from './../assets/projets/ohmyfood.png'
+import lachouette from './../assets/projets/lachouette.png'
+import kanap from './../assets/projets/kanap.png'
+import groupomania from './../assets/projets/groupomania.png'
+import tipcal from './../assets/projets/tipcal.png'
+import todo from './../assets/projets/todo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLaptopCode } from '@fortawesome/free-solid-svg-icons'
 
 const Portfolio = () => {
     return (
-        <div>
-            <h2 id='portfolio'>Porfolio</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aperiam nam tempore, labore ut quaerat corporis quam totam dolor, dolores delectus nostrum soluta nihil animi dicta consequatur officiis numquam ullam.
-            Illo pariatur debitis dicta minima quasi? Obcaecati molestiae explicabo iure delectus impedit omnis debitis nam iusto? Error eveniet delectus dignissimos enim est quasi quam explicabo ut. Tempora sapiente enim id.
-            Eligendi debitis esse cumque cum, enim, atque, sint dolores officiis velit animi tenetur! Voluptates, sunt illum vitae neque ab laborum quos corrupti? Sequi adipisci corporis ratione corrupti ad quo molestias.
-            Dolore accusamus voluptatibus dicta eveniet laudantium, fuga perferendis aut deleniti molestiae enim, veritatis sed provident sequi maiores atque id. Sunt blanditiis labore dignissimos veniam vitae assumenda obcaecati illum quidem nihil!</p>
-        </div>
+        <section id='portfolio' className='section'>
+            <div>
+                <h2>Portfolio<span>mes projets</span></h2>
+                <div className="separation-line"></div>
+
+                <div className='portfolio-container'>
+                    <figure  className='portfolio-projet'>
+                        <img src={booki} alt="" />
+                        <figcaption>
+                            <p>HTML & CSS</p>
+                            <h3>Booki</h3>
+                            <p>Transformer une maquette en site web avec HTML & CSS.</p>
+                            <a target="_blank" rel='noreferrer' href="https://celinedev.github.io/booki/">Démo du projet <FontAwesomeIcon icon={faLaptopCode} size="lg" /></a>
+                        </figcaption>
+                    </figure>
+                    <figure  className='portfolio-projet'>
+                        <img src={ohmyfood} alt="" />
+                        <figcaption>
+                            <p>HTML & SASS</p>
+                            <h3>Ohmyfood</h3>
+                            <p>Transformer une maquette en site web avec des animations en mobile-first.</p>
+                            <a target="_blank" rel='noreferrer' href="https://celinedev.github.io/Ohmyfood/">Démo du projet <FontAwesomeIcon icon={faLaptopCode} size="lg" /></a>
+                        </figcaption>
+                    </figure>
+                    <figure  className='portfolio-projet'>
+                        <img src={lachouette} alt="" />
+                        <figcaption>
+                            <p>SEO, accessibilité</p>
+                            <h3>La chouette agence</h3>
+                            <p>Amélioration du SEO et de l'accessibilité d'un site web.</p>
+                            <a target="_blank" rel='noreferrer' href="https://celinedev.github.io/lachouetteagence.github.io/index.html">Démo du projet <FontAwesomeIcon icon={faLaptopCode} size="lg" /></a>
+                        </figcaption>
+                    </figure>
+                    <figure  className='portfolio-projet'>
+                        <img src={kanap} alt="" />
+                        <figcaption>
+                            <p>Javascript</p>
+                            <h3>Kanap</h3>
+                            <p>Réalisation de la partie Javascript d'un site e-commerce dont le back et l'API sont fournit.</p>
+                            <a title='github' target="_blank" rel='noreferrer' href="https://github.com/celineDev/kanap">Repo Github du projet <FontAwesomeIcon icon={faSquareGithub} size="lg" /></a>
+                        </figcaption>
+                    </figure>
+                    <figure  className='portfolio-projet'>
+                        <img src={groupomania} alt="" />
+                        <figcaption>
+                            <p>React JS</p>
+                            <h3>Groupomania</h3>
+                            <p>Construire un réseau social interne entreprise.</p>
+                            <a title='github' target="_blank" rel='noreferrer' href="https://github.com/celineDev/groupomania_22022022">Repo Github du projet <FontAwesomeIcon icon={faSquareGithub} size="lg" /></a>
+                        </figcaption>
+                    </figure>
+                    <figure  className='portfolio-projet'>
+                        <img src={tipcal} alt="" />
+                        <figcaption>
+                            <p>Javascript</p>
+                            <h3>Tip Calculator App</h3>
+                            <p>Application qui calcule le pourboire en fonction du prix, du nombres de personnes et du pourcentage à verser.</p>
+                            <a target="_blank" rel='noreferrer' href="https://github.com/celineDev/kanap">Démo du projet <FontAwesomeIcon icon={faLaptopCode} size="lg" /></a>
+                        </figcaption>
+                    </figure>
+                    <figure  className='portfolio-projet'>
+                        <img src={todo} alt="" />
+                        <figcaption>
+                            <p>Javascript</p>
+                            <h3>Todo App</h3>
+                            <p>Application de liste to-do.</p>
+                            <a target="_blank" rel='noreferrer' href="https://celinedev.github.io/To-do-app/">Démo du projet <FontAwesomeIcon icon={faLaptopCode} size="lg" /></a>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        </section>
     );
 };
 
