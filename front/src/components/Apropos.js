79253ba418efd78c8b18c35b227870017d097f72
Celine Mouflard <celine667@hotmail.fr>
2023-03-13 16:57:42 +0100

    few css refactoring

--- front/src/components/Apropos.js
+++ front/src/components/Apropos.js
@@ -3,13 +3,15 @@ import cv from './../assets/cv/cv.pdf'
 
 const Apropos = () => {
     return (
-        <section id="apropos">
+        <section id="apropos" className='section'>
             <h2>à-propos de moi<span>qui suis-je ?</span></h2>
             <div className="separation-line"></div>
-            <p>Passionnée par le développement web, je suis particulièrement intéressée par le front-end et le framework React JS. Pour élargir mes compétences, je continue à me former quotidiennement sur ce framework.
-            J'ai comme ambition de progresser et d'élargir mes compétences afin de rester à la pointe de l'innovation en terme de compétences.
-            </p>
-            <button><a target="_blank" rel="noreferrer" href={cv}>Télécharger mon CV</a></button>
+            <div className='container-apropos'>
+                <p>Passionnée par le développement web, je suis particulièrement intéressée par le front-end et le framework React JS. Pour élargir mes compétences, je continue à me former quotidiennement sur ce framework.
+                J'ai comme ambition de progresser et d'élargir mes compétences afin de rester à la pointe de l'innovation en terme de compétences.
+                </p>
+                <button><a target="_blank" rel="noreferrer" href={cv}>Télécharger mon CV</a></button>
+            </div>
         </section>
     );
 };
